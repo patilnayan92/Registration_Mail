d296862b07f8d99d1df9d071b79b987d9b0d5ea6
Nayan Patil <34345513+patilnayan92@users.noreply.github.com>
2018-01-07 20:12:37 +0530

    Update urls.py

--- temail/urls.py
+++ temail/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.signup),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 ]
